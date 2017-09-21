101c05eedf14b6cb44a393d56e0bf6c107569fab
Liam Ryan <liamryandev@gmail.com>
2017-09-21 20:18:19 +0100

    Commented out nofork

--- test/test.c
+++ test/test.c
@@ -521,7 +521,9 @@ int main() {
 
     s = list_suite();
     sr = srunner_create(s);
-    srunner_set_fork_status(sr, CK_NOFORK);
+
+//    Uncomment if you wish to debug unit tests or run valgrind on this file
+//    srunner_set_fork_status(sr, CK_NOFORK);
 
     srunner_run_all(sr, CK_NORMAL);
     number_failed = srunner_ntests_failed(sr);
